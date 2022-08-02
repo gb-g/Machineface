f08e64c3c6fdbd80a77a57e095c7aa600c317ba5
Gil Benkö <38266358+GilBenkoe@users.noreply.github.com>
2022-08-02 12:48:00 +0200

    i18n: Add French translation

---
+++ translations/machineface_fr.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <source>About Machineface</source>
+        <translation>À propos de Machineface</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>ApplicationMenu</name>
+    <message>
+        <source>Menu</source>
+        <translation>Menu</translation>
+    </message>
+    <message>
+        <source>&amp;Disconnect</source>
+        <translation type="vanished">Déconnecter</translation>
+    </message>
+    <message>
+        <source>Sh&amp;utdown</source>
+        <translation type="vanished">Arrêter</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation type="vanished">Quitter</translation>
+    </message>
+    <message>
+        <source>&amp;Disconnect from Session</source>
+        <translation>Déconnecter de la session</translation>
+    </message>
+    <message>
+        <source>&amp;About Machineface</source>
+        <translation>À propos de Machineface</translation>
+    </message>
+    <message>
+        <source>Sh&amp;utdown Session</source>
+        <translation>Arrêter la session</translation>
+    </message>
+    <message>
+        <source>E&amp;xit User Interface</source>
+        <translation>Quitter interface utilisateur</translation>
+    </message>
+</context>
+<context>
+    <name>ButtonJog</name>
+    <message>
+        <source>Move </source>
+        <translation>Bouger </translation>
+    </message>
+    <message>
+        <source> and </source>
+        <translation> et </translation>
+    </message>
+    <message>
+        <source> axis to 0</source>
+        <translation> axe à 0</translation>
+    </message>
+    <message>
+        <source>Select axis action</source>
+        <translation>Sélectionner action axe</translation>
+    </message>
+    <message>
+        <source>Select Action</source>
+        <translation>Sélectionner axe</translation>
+    </message>
+    <message>
+        <source>Touch off </source>
+        <translation>Touch off </translation>
+    </message>
+    <message>
+        <source> axis</source>
+        <translation> axe</translation>
+    </message>
+    <message>
+        <source>Change extruder</source>
+        <translation>Changer extrudeuse</translation>
+    </message>
+    <message>
+        <source>Select extruder</source>
+        <translation>Sélectionner extrudeuse</translation>
+    </message>
+    <message>
+        <source>Extruder </source>
+        <translation>Extrudeuse </translation>
+    </message>
+    <message>
+        <source>Velocity</source>
+        <translation>Vélocité</translation>
+    </message>
+</context>
+<context>
+    <name>DisplayPanel</name>
+    <message>
+        <source>Heated Bed</source>
+        <translation>Plateau chauffé</translation>
+    </message>
+    <message>
+        <source>Heated Chamber</source>
+        <translation>Enceinte chauffée</translation>
+    </message>
+    <message>
+        <source>Extruder %1</source>
+        <translation>Extrudeuse %1</translation>
+    </message>
+    <message>
+        <source>Fan %1</source>
+        <translation>Ventilateur %1</translation>
+    </message>
+    <message>
+        <source>Light %1</source>
+        <translation>Lumière %1</translation>
+    </message>
+</context>
+<context>
+    <name>GCodeTab</name>
+    <message>
+        <source>GCode</source>
+        <translation>GCode</translation>
+    </message>
+</context>
+<context>
+    <name>GantryConfigControl</name>
+    <message>
+        <source>Left offset:</source>
+        <translation>Décalage gauche:</translation>
+    </message>
+    <message>
+        <source>Right offset:</source>
+        <translation>Décalage droit:</translation>
+    </message>
+</context>
+<context>
+    <name>JogControlTab</name>
+    <message>
+        <source>Jog</source>
+        <translation>Jog</translation>
+    </message>
+</context>
+<context>
+    <name>JogVelocityKnob</name>
+    <message>
+        <source>Set %1 axis velocity</source>
+        <translation>Ajuster velocité axe %1</translation>
+    </message>
+</context>
+<context>
+    <name>LightControl</name>
+    <message>
+        <source>Pick Color</source>
+        <translation>Sélectionner couleur</translation>
+    </message>
+</context>
+<context>
+    <name>MdiTab</name>
+    <message>
+        <source>MDI</source>
+        <translation>Saisie manuelle (MDI)</translation>
+    </message>
+</context>
+<context>
+    <name>OverlayDialog</name>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+</context>
+<context>
+    <name>PreviewTab</name>
+    <message>
+        <source>Preview</source>
+        <translation>Aperçu</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsTab</name>
+    <message>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <source>Digital Read Out</source>
+        <translation>Affichage numérique (DRO)</translation>
+    </message>
+    <message>
+        <source>Show offsets</source>
+        <translation>Afficher décalages</translation>
+    </message>
+    <message>
+        <source>Show velocity</source>
+        <translation>Afficher vélocité</translation>
+    </message>
+    <message>
+        <source>Show distance to go</source>
+        <translation>Afficher distance restante</translation>
+    </message>
+    <message>
+        <source>Other</source>
+        <translation>Autre</translation>
+    </message>
+    <message>
+        <source>Enable preview</source>
+        <translation>Activer aperçu</translation>
+    </message>
+    <message>
+        <source>Maximum Velocity:</source>
+        <translation>Vélocité max:</translation>
+    </message>
+</context>
+<context>
+    <name>TemperatureControl</name>
+    <message>
+        <source>N/A</source>
+        <translation>N/A</translation>
+    </message>
+</context>
+<context>
+    <name>ToggleSettingCheck</name>
+    <message>
+        <source>Group Value</source>
+        <translation type="vanished">Group value</translation>
+    </message>
+</context>
+<context>
+    <name>VelocityExtrusionControl</name>
+    <message>
+        <source>Velocity Extrusion</source>
+        <translation>Vélocité extrusion</translation>
+    </message>
+    <message>
+        <source>Filament diameter:</source>
+        <translation>Diamètre filament:</translation>
+    </message>
+    <message>
+        <source>Retract velocity:</source>
+        <translation>Vélocité retrait:</translation>
+    </message>
+    <message>
+        <source>Retract length:</source>
+        <translation>Distance retrait:</translation>
+    </message>
+    <message>
+        <source>Extrude scale:</source>
+        <translation>Échelle extrusion:</translation>
+    </message>
+    <message>
+        <source>Accel. adj. gain:</source>
+        <translation>Ajustage gain accélération:</translation>
+    </message>
+</context>
+<context>
+    <name>VideoTab</name>
+    <message>
+        <source>Video</source>
+        <translation>Vidéo</translation>
+    </message>
+    <message>
+        <source>Webcam not available</source>
+        <translation>Webcam non disponible</translation>
+    </message>
+</context>
+</TS>
